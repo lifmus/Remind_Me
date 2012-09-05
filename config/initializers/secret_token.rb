9172b3100ede8d2039319b1f4a3c4c455263e944
Andrew Lin <f50ajl@gmail.com>
2012-09-05 15:46:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemindMe::Application.config.secret_token = '9c21be685d0a41476e2cfc626683e45eda7f46c186c8b3722b86a28c97fca07da90c8ec65715f93456082791e7078903d0766bfe6992960f508e4eac2ac1f9c6'
